258afadc97efdbdab868c07180919e2d8065d857
Prabesh <prabesh.bista369@gmail.com>
2021-11-28 10:35:54 +0545

    opperators& Comments

---
+++ day4/airthmetic_operations.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+ 
+int main(){
+    int a=20;
+    int b=40;
+    cout<<"a+b:"<<a+b<<endl;//+ is an addition operator
+    cout<<"a-b:"<<a-b<<endl;//- is a subtraction operator
+    cout<<"a*b:"<<a*b<<endl;// * is a multiplication operator
+    cout<<"b/a:"<<b/a<<endl;// / is a division operator
+    cout<<"a%b:"<<a % b<<endl; // % is a modulo operator
+
+}
