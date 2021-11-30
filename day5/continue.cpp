124cef9a09cc31e32a6f39c2afc2a102005866d9
Prabesh <prabesh.bista369@gmail.com>
2021-11-30 15:20:27 +0545

    continue statement

--- day5/continue.cpp
+++ day5/continue.cpp
@@ -2,6 +2,30 @@
 using namespace std;
 
 int main(){
-    cout<<"Hello my name is Prabesh Bista";
-    
+    for (int i=1;i<=7;++i){
+        if(i==2 || i==6){
+            continue; // continue statement
+        }
+    cout<<i<<endl;
+    }
+    int num=0;
+    int sum=0;
+
+    while(num>=0){
+        sum =sum+num;
+        cout<<"Enter the Number:";
+        cin>>num;
+        
+        if(num>60){
+            
+            cout<<"Number should be less than 60"<<endl;
+            num=0;
+            continue;
+        }
+         
+     }
+
+    cout<<"total sum of enter number is:"<<sum
+    <<endl;
+
 }
