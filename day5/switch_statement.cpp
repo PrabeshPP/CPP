a01301c818f5a8035958737732b9cfdb29ee08ab
Prabesh <prabesh.bista369@gmail.com>
2021-11-30 17:18:39 +0545

    switch_statement

--- day5/switch_statement.cpp
+++ day5/switch_statement.cpp
@@ -2,7 +2,40 @@
 using namespace std;
  
 int main(){
-    cout<<"My names is Prabesh Bista";
+    char oper;
+    int num1,num2;
+    cout<<"Enter an operator from the list  {+,-,*,/,%} :";
+    cin>>oper;
+    cout<<"Enter two numbers";
+    cin>>num1>> num2;
+
+    // switch statement :Note*(char has to be inside a single quotation like '')
+
+    switch (oper)
+    {
+    case '+':
+        cout<<"Sum of two number is :"<<(num1+num2)<<endl;
+        break;
+    case '-':
+        cout<<"Difference of two number is:"<<(num1-num2)<<endl;
+        break;
+    case '*':
+        cout<<"Product of two number is :"<<(num1*num2)<<endl;
+        break;
+
+    case '/':
+        cout<<"Division of two number is :"<<(num1/num2)<<endl;
+        break;
+    case '%':
+    cout<<"Modulo division of two number is :"<<num1%num2<<endl;
+    break;
+
+
     
+    default:
+    cout<<"Error!The operator is not correct:";
+        break;
+    }
+
 
 }
