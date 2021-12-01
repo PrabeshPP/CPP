fe08c15de2354ed564c8a5697a84d386f1fdb62b
Prabesh <prabesh.bista369@gmail.com>
2021-12-01 09:37:06 +0545

    void function

---
+++ day6/funtion.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void printNum(int num){
+    cout<<num;
+}
+
+int main(){
+    int num;
+cout<<"Enter a number that you want to print:";
+cin>>num;
+printNum(num);
+}
+
