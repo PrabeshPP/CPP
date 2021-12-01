7e896bd499621e5d83b7ffc9ec4a8c89a4c5cf96
Prabesh <prabesh.bista369@gmail.com>
2021-12-01 10:15:15 +0545

    learned prototype function & c++ funct.

--- day6/prototype_function.cpp
+++ day6/prototype_function.cpp
@@ -1,21 +1,34 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 
 /* in c++ code  function should be defined before call.
 But if we want to define the function after the call,we should use function prototype
 */
+void greeting();
 void addNumbers(int num1,int num2);
+void Square(int num11){
+int Sqrt=sqrt(num11); // C++ Square Root Function
+cout<<"Square root of the number is:"<<Sqrt;
+}
+
 
 int main(){
     int number1,number2;
+greeting();
 cout<<"Enter the first number:";
 cin>>number1;
-cout<<"Enter the second number";
+cout<<"Enter the second number:";
 cin>>number2;
 addNumbers(number1,number2);
+Square(number1);
 }
 
 void addNumbers(int num1,int num2){
 int sum=num1+num2;
 cout<<"The sum of two numbers is :"<<sum<<endl;
+}
+
+void greeting(){
+    cout<<"Welcome to C++ tutorial of using function prototype!"<<endl;
 }
