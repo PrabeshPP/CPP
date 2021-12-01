50d89c9ec373ab5d50eccb7f3687e70d47b506ed
Prabesh <prabesh.bista369@gmail.com>
2021-12-01 09:37:17 +0545

    void function

--- day6/funtion.cpp
+++ day6/funtion.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 using namespace std;
 
-void printNum(int num){
-    cout<<num;
+void printNum(int num1,float num2){
+   cout<<"The integer is :"<<num1<<endl;
+   cout<<"The floating number is :"<<num2<<endl;
 }
 
 int main(){
-    int num;
-cout<<"Enter a number that you want to print:";
-cin>>num;
-printNum(num);
+    int num1;
+    float num2;
+cout<<"Enter the integer and floating number:";
+cin>>num1>>num2;
+
+printNum(num1,num2);
 }
 
