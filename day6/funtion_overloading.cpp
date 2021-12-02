e6ee1ba8d609e74dd5140eb48c64379abcca7190
Prabesh <prabesh.bista369@gmail.com>
2021-12-02 15:32:21 +0545

    learned function overloading

---
+++ day6/funtion_overloading.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int absolute(int a){
+    if(a<0.0){
+        a=-a;
+
+    }
+    return a;
+}
+float absolute(float a){
+    if(a<0){
+        a=-a;
+
+    }
+    return a;
+}
+
+// int main(){
+//     cout<<"Absolute value of -5="<<absolute(-5)<<endl;
+//     cout<<"Absolute value of -5.55="<<absolute(-5.982)<<endl;
+
+
+// }
+
+
+
+
