a734e3b93c2e42988da8610750d2e6ede18a4ea6
Prabesh <prabesh.bista369@gmail.com>
2021-12-02 15:32:27 +0545

    learned function overloading

--- day6/funtion_overloading.cpp
+++ day6/funtion_overloading.cpp
@@ -1,13 +1,6 @@
 #include <iostream>
 using namespace std;
-
-int absolute(int a){
-    if(a<0.0){
-        a=-a;
-
-    }
-    return a;
-}
+// overloading using different parameters
 float absolute(float a){
     if(a<0){
         a=-a;
@@ -16,12 +9,34 @@ float absolute(float a){
     return a;
 }
 
-// int main(){
-//     cout<<"Absolute value of -5="<<absolute(-5)<<endl;
-//     cout<<"Absolute value of -5.55="<<absolute(-5.982)<<endl;
+int absolute(int a){
+    if(a<0.0){
+        a=-a;
+
+    }
+    return a;
+}
+// overloadinf using Different numbers of parameters
+
+void display(int a,double b){
+    int sum=0;
+    sum=a+b;
+    cout<<"The sum of two numbers is :"<<sum<<endl;
+}
+
+void display(int a){
+    cout<<"The passed integer is :"<<a<<endl;
+}
 
 
-// }
+int main(){
+    cout<<"Absolute value of -5="<<absolute(-5)<<endl;
+    cout<<"Absolute value of -5.55="<<absolute(-5.982f)<<endl;
+    display(20,40);
+    display(20);
+
+
+}
 
 
 
