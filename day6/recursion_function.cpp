e2b4eb3b63113b4627c32c6228459cff8ce5e9b9
Prabesh <prabesh.bista369@gmail.com>
2021-12-02 16:13:44 +0545

    learned recursive function

--- day6/recursion_function.cpp
+++ day6/recursion_function.cpp
@@ -1,13 +1,22 @@
 # include <iostream>
 using namespace std;
 
-int itr(int a){
-    int var=1;
-    var=var*itr(a-1);
-    return a;
+int itr1(int a){
+    int var;
+if(a>1){
+    var=a*itr1(a-1);
+    return var;
+
+}else{
+    return 1;
+}
+
 }
 
 int main(){
-int value=itr(3);
-cout<<value<<endl;
+int value,n;
+cout<<"Enter the number for its factorial :";
+cin>>n;
+value=itr1(n);
+cout<<"The factorial of " <<n<< " is " <<value<<endl;
 }
