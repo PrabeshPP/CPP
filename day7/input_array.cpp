bf44aaa6147a0e02987d67a6ff24bab94c7a1088
Prabesh <prabesh.bista369@gmail.com>
2021-12-03 10:39:02 +0545

    learned abt. ranged based loop,input_array

--- day7/input_array.cpp
+++ day7/input_array.cpp
@@ -12,4 +12,21 @@ cout<<"All the six nummbers of any array is:";
     for (const int &n:numbers){ // using range based loop
 cout<<n<<" "; // printing all the value of an array
     }
+
+// program to calulate sum and average of an elements of an array
+double sum=0;
+double count=0;
+double average=0;
+
+for (const int &i:numbers){
+    sum+=i;
+    ++count;
+    
+}
+average=(sum/count);
+
+cout<<"The average of an array is:"<<average;
+
+
+
 }
