71cac8af4f908c00a7a435bdcd0a03908dbc4351
Prabesh <prabesh.bista369@gmail.com>
2021-12-04 10:46:49 +0545

    worked on passing the array to function

--- day7/function_array.cpp
+++ day7/function_array.cpp
@@ -3,10 +3,23 @@ using namespace std;
 
 //passing the single-dimensional and multi-dimensional array
 
+//displaying the 1D array
 void display(int m[5]){
     cout<<"Displaying the marks:";
     for (int i=0;i<5;++i){
         cout<<m[i]<<" ";
+    }
+    cout<<"\n";
+
+}
+
+//displaying the 2d array
+void display1(int n2[2][3]){
+    for(int i=0;i<2;++i){
+        for(int j=0;j<3;++j){
+            cout<<n2[i][j]<<" ";
+        }
+        cout<<"\n";
     }
 }
 
@@ -14,5 +27,10 @@ int main(){
 // passing the 1D array
 int marks[5]={90,20,50,100,80};
 display(marks);
+
+//passing the 2D array
+int marks1[2][3]={{1,2,3},{4,5,6}};
+display1(marks1);
+
 
 }
