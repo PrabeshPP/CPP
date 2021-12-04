1b481fbb8d9e6ce24beaa11e34f6087bcb98ecc6
Prabesh <prabesh.bista369@gmail.com>
2021-12-04 09:39:34 +0545

    learned about ranged loop,& ref

--- day7/rangebased_loop.cpp
+++ day7/rangebased_loop.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 // range based loop
-int numbers[]={1,2,3,4,6};
+int numbers[5]={1,2,3,4,6};
+vector<int > num_vector={22,33,44,55,66};
 int main(){
-    for(int num:numbers){
+    cout<<"The numbers of an array are:";
+    for(int num:numbers){ //the num range declaration would store value each time numbers array is iterate
         cout<<num<<" ";
 
     }
+    
+    
+    cout<<"\nThe numbers of the vector are:";
+    for (int &var:num_vector){ //using the & would reference the memory address and rather than copying it
+        cout<<var<<" ";
+    }
+
 
 }
