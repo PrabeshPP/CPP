3b8f091e16d4a4c8a856b440f0c94c88866d8eef
Prabesh <prabesh.bista369@gmail.com>
2021-12-04 10:13:01 +0545

    worked on 2d and input for 2d array

---
+++ day7/multidimensional_array.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // initializing two dimensional array
+    int test[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+    for(int i=0;i<2;++i){
+        for(int j=0;j<2;++i){
+            cout<<test[i][j];
+        }
+    }
+}
