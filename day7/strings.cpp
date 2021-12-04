a6f08bf1c75c13056b40dba424fc45e933de0b19
Prabesh <prabesh.bista369@gmail.com>
2021-12-04 12:32:16 +0545

    C-string

--- day7/strings.cpp
+++ day7/strings.cpp
@@ -4,5 +4,21 @@ using namespace std;
 int main(){
     // C-string
     char str[]="C++"; //the string would be terminated by a null character(\0)
-    cout<<str[0];
+    cout<<str[0]<<endl;
+    /* taking input from the user.
+    However,if there is space between the character only the string before space will get print
+     */
+    char str1[100];
+    char str3[100];
+    cout<<"Enter the string :";
+    cin>>str1;
+    cout<<str1;
+    cout<<"\n";
+    // C++ string to read a line of text
+    
+    cout<<"Enter the string with space:";
+    cin.get(str3,100);
+    cout<<"\n";
+    cout<<"You entered "<<str3<<"string ";
+
 }
