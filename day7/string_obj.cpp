c25912ed59a1580b09931dadec9574bc57181d97
Prabesh <prabesh.bista369@gmail.com>
2021-12-04 12:45:50 +0545

    string_obj and passing it as arg to function

---
+++ day7/string_obj.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+void display(char*);
+void display(string);
+
+// string object
+int main(){
+    string str;
+    char str1[100];
+    cout<<"Enter the string :";
+    getline(cin,str); // takes input with and without space
+    cout<<"Enter the char string :";
+    cin.get(str1,100); 
+    cout<<"\n";
+    display(str1);
+    display(str);
+   
+
+
+}
+void display(string str){
+    cout<<"Entered string is "<<str<<endl;
+}
+void display(char s[]){
+    cout<<"Entered char array is "<<s<<endl;
+}
