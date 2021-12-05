d7e004a0689e1f4d4a32a7ebcdcaf9de4ef79378
Prabesh <prabesh.bista369@gmail.com>
2021-12-05 09:31:56 +0545

    structures in C++

--- day8/structures.cpp
+++ day8/structures.cpp
@@ -1,6 +1,27 @@
 #include<iostream>
 using namespace std;
+struct Person{
+    char name[50];
+    int age;
+    float salary;
+
+};
 
 int main(){
-    
+    Person person;
+    cout<<"Enter the full name:";
+    cin.get(person.name,50);
+    cout<<"Enter the age:";
+    cin>>(person.age);;
+    cout<<"Enter the salary:";
+    cin>>(person.salary);
+
+    //printing the value of the structure of the person
+    cout<<"The name of the person is "<<person.name<<endl;
+    cout<<"The age of the person is"<<person.age<<endl;
+    cout<<"The salary of the person is "<<person.salary<<endl;
+
+
+
+
 }
