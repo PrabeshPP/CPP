d9cea630a4bd562e728cfb7eeac7f6c3470dcf13
Prabesh <prabesh.bista369@gmail.com>
2021-12-05 10:12:10 +0545

    return struct and funct

--- day8/struct_function.cpp
+++ day8/struct_function.cpp
@@ -9,22 +9,31 @@ struct struct_function
 };
 
 
-
-void display(struct_function st);
+struct_function getData(struct_function st);
+void display(struct_function st); //we have to define which type of argument we are going to pass
 int main(){
     struct_function st;
-    cout<<"Enter the name of the person:";
-    getline(cin,st.name);
-    cout<<"Enter the age of the person:";
-    cin>>st.age;
-    cout<<"Enter the salary of the person";
-    cin>>st.salary;
-    display(st);
+    st=getData(st);
+    
+    display(st); // passing the structur data type in the display function
 
 
 }
 
 void display(struct_function st){
-    cout<<"The name of the person is "<<st.name<<",and the age is "<<st.age<<"\n ";
-    cout<<"The salary of the person is"<<st.salary<<endl;
+    cout<<"The name of the person is "<<st.name<<",and the age is "<<st.age<<"."<<endl;
+    cout<<"The salary of the person is "<<st.salary<<endl;
+}
+
+struct_function getData(struct_function st){
+    cout<<"Enter the Full-Name:";
+    getline(cin,st.name);
+    cout<<"Enter the Age:";
+    cin>>st.age;
+    cout<<"Enter the Salary:";
+    cin>>st.salary;
+
+    return st;
+
+
 }
