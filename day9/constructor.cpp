6e483b8794121555a7d71b8cad7dee7c998c7978
Prabesh <prabesh.bista369@gmail.com>
2021-12-06 17:54:44 +0545

    constructor,cin

---
+++ day9/constructor.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+
+class Wall{
+    private:
+        double length;
+        double breadth;
+    public:
+        Wall(double len,double bred    ){  // constructor of the class "Area"
+        length=len;
+        breadth=bred;
+
+        }
+
+        double calculateArea(){
+        return length*breadth;
+        }
+};
+
+int main(){
+    double l,b;
+
+  
+    cout<<"Enter the length of the wall :";
+    cin>>l;
+    cout<<"Enter the breadth of the wall :";
+    cin>>b;
+   
+    Wall wall1(l,b);
+    cout<<"\n";
+    cout<<"The area of the wall is "<<wall1.calculateArea();
+
+
+
+}
