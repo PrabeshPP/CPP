aa22ab8b105373c2ecf9a43a4f6710884128e65c
Prabesh <prabesh.bista369@gmail.com>
2021-12-06 13:07:17 +0545

    learned pointers and struct

--- day8/pointers_structure.cpp
+++ day8/pointers_structure.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+struct Distance
+{
+int feet;
+float  inch;
+};
 
-    cout<<"Hellow World";
+int main(){
+    Distance *ptr,d;
+    ptr=&d;
+    cout<<"Enter feet";
+    cin>>ptr->feet;
+    cout<<"Enter the inch";
+    cin>>ptr->inch;
+    cout<<"Feet is "<<(*ptr).feet<<endl;
+    cout<<"Inch is "<<(*ptr).inch<<endl;
 }
