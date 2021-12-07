c615ed8e2371a3974986d1e0ca7870c480fb4f24
Prabesh <prabesh.bista369@gmail.com>
2021-12-07 21:25:45 +0545

    pointer&array

--- day10/pointer_array.cpp
+++ day10/pointer_array.cpp
@@ -2,11 +2,36 @@
 using namespace std;
 
 int main(){
-    // sotoring thr address of the first element of an array
-    int* ptr;
     int arr[5];
-    ptr=arr; //refrencing address of the first element of an array in pointer
-    cout<<ptr<<endl;
-    ptr=arr+1; //arr+1 is equal to &arr[1]
-    cout<<ptr;
+    int* ptr;
+    cout<<"Displaying the address of the array cells using & :";
+    for (int i=0;i<5;++i){
+        cout<<"The address of the "<<i<<"th element is "<<&arr[i]<<endl;
+    }
+
+    cout<<"Displaying the address of the array elements using pointer :";
+    ptr=arr; //storing the address of the first element of an array
+    for (int i=0;i<5;++i){
+        cout<<"The address of the "<<i<<"th element is "<<ptr+i<<endl;
+    }
+
+
+    //Array name used as pointer
+    float arr1[6];
+    cout<<"Enter the six numbers :"<<endl;
+    for(int i=0;i<6;++i){
+        cin>>*(arr+i);
+
+    }
+
+    cout<<"The six numbers are :";
+    for(int i=0;i<6;++i){
+        if(i!=5){
+        cout<<*(arr+i)<<" , ";
+        }else{
+            cout<<*(arr+i)<<" . ";
+        }
+        
+        
+    }
 }
