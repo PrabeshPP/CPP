091a34fa871afb87f254d1fedad62858c32fdb5a
Prabesh <prabesh.bista369@gmail.com>
2021-12-07 19:23:35 +0545

    worked on pointers,changin the value usin pointers

--- day10/pointer.cpp
+++ day10/pointer.cpp
@@ -2,13 +2,20 @@
 using namespace std;
 
 int main(){
+    int* pointVar; //initializing thr pointer 
     int var1=10;
+    pointVar=&var1; //assigning the address of the var1 to the pointer
+  cout<<"The address of the variable1 is "<<*pointVar<<endl;
     int var2=20;
-    int var3=40;
+    pointVar=&var2;
+    cout<<"The address of the variable2 is "<<*pointVar<<endl;
 
-    cout<<"The address of the variable1 is "<<&var1<<endl;
-    cout<<"The address of the variable2 is "<<&var2<<endl;
-    cout<<"The address of the variable3 is "<<&var3<<endl;
+    int var3=40;
+    pointVar=&var3;
+   cout<<"The address of the variable3 is "<<*pointVar<<endl;
+
+   *pointVar=2; //changing the value of var3 using the pointer
+   cout<<"The new value of the variable 3 is "<<var3<<endl; //var3 will print new value
 
 
 }
