8e6eaa645380a8733c4013022a7bd76479d6166e
Prabesh <prabesh.bista369@gmail.com>
2021-12-07 21:48:34 +0545

    functions,pointers,swapvalue

--- day10/pointers_function.cpp
+++ day10/pointers_function.cpp
@@ -10,6 +10,9 @@ void swap(int &n1,int &n2){
     n2=temp;
 }
 
+//passing by reference with pointers and using function prototype
+void swap1(int*, int*);
+
 
 int main(){
     int a=1,b=20;
@@ -18,4 +21,17 @@ int main(){
     cout<<"After swaping the value :";
     swap(a,b);
     cout<<a<<","<<b;
+    cout<<"\n";
+    int var1=200,var2=600;
+    cout<<"Result after swaping the variable using pointers:";
+    swap1(&var1,&var2);
+    cout<<var1<<","<<var2;
+}
+
+void swap1(int* n11,int* n22){
+    int temp;
+    temp=*n11;
+    *n11=*n22;
+    *n22=temp;
+
 }
