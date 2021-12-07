67801f8cce55a31e12d6af713c91dc2994b2de65
Prabesh <prabesh.bista369@gmail.com>
2021-12-07 19:23:32 +0545

    worked on pointers,changin the value usin pointers

---
+++ day10/pointer.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int var1=10;
+    int var2=20;
+    int var3=40;
+
+    cout<<"The address of the variable1 is "<<&var1<<endl;
+    cout<<"The address of the variable2 is "<<&var2<<endl;
+    cout<<"The address of the variable3 is "<<&var3<<endl;
+
+
+}
