e30358db473955a0ea2441f240f0059868567fba
Prabesh <prabesh.bista369@gmail.com>
2021-12-08 09:26:19 +0545

    memory_management

--- day10/memory_management.cpp
+++ day10/memory_management.cpp
@@ -5,6 +5,21 @@ using namespace std;
 //c++ new operator
 // use new to allocate the memory address 
 // we should use delete to deallocate the memory address
+
+
+//c++ new and delete operator for Objects
+
+class Student{
+    int age;
+    public:
+    Student():age(12){}
+    void getAge(){
+        cout<<"Age="<<age<<endl;
+    }
+
+};
+
+void array_fun();
 int main(){
     int* pointVar;
     //dynamically allocating memory
@@ -14,6 +29,34 @@ int main(){
     cout<<*pointVar;
     //deallocate the memory
     delete pointVar;
-    
+    cout<<'\n';
+    array_fun();
+
+    Student* ptr11=new Student();
+    ptr11->getAge();
+    delete ptr11;
+
+
+
+}
+// c++ new and delete[] operator for an array
+void array_fun(){
+    int num;
+    cout<<"Enter the number of students : ";
+    cin>>num;
+    float* ptr;
+    ptr=new float[num];
+    cout<<"Enter the GPA of students :"<<endl;
+    for (int i=0;i<num;++i){
+cout<<"Stduents "<<i+1<<":";
+cin>>*(ptr+i);
+    }
+    cout<<"\n Displaying GPA of students:"<<endl;
+    for (int j=0;j<num;++j){
+        cout<<"GPA of Students "<< j+1<<" : "<< *(ptr+j) << ""<<endl;
+    }
+
+delete[] ptr;
+
 
 }
