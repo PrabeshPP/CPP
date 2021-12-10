74f2f0bf1d91806ba173b1d3498e9562b001ca00
Prabesh <prabesh.bista369@gmail.com>
2021-12-10 15:28:27 +0545

    pointer,scope to call overridden function

---
+++ day11/pointer_overridden.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+class BaseClass{
+    public:
+    void output(){
+        cout<<"Hello my name is Prabesh Bista. \n I am a mobile app developer.\n";
+    }
+};
+
+class DerivedClass:public BaseClass{
+    public:
+    void output(){
+        cout<<"This is a derived class."<<endl;
+
+    }
+};
+
+//accessing the overridden function using pointer in c++
+
+int main(){
+    DerivedClass  derivedclass;
+    // pointer of the base type that points to derivedclass
+    BaseClass* ptr=&derivedclass;
+    ptr->output();
+
+}
