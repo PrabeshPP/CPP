5440b8317c10bce71a6cef2c3f3fa24a74335e33
Prabesh <prabesh.bista369@gmail.com>
2021-12-10 20:52:34 +0545

    soln3

---
+++ problems/soln3.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<numeric>
+using namespace std;
+
+
+void problem(){
+    int n;
+    cin>>n;
+    int arr[3];
+    int soln=0;
+    int sum=0;
+    for (int i=0;i<n;++i){
+       
+        for (int j=0;j<3;++j){
+            cin>>arr[j];
+        }
+       sum= accumulate(arr,arr+3,sum);
+       if(sum>=2){
+          soln+=1;
+       }
+        sum=0;
+    }
+    
+    cout<<soln;
+}
+
+
+int main(){
+    problem();
+    
+}
