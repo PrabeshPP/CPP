6a48a685276e34d77ddcb0626fbcc4ce35c4d344
Prabesh <prabesh.bista369@gmail.com>
2021-12-18 10:35:40 +0545

    virtual function exp2

---
+++ day/virtual_class.cpp
@@ -0,0 +1,50 @@
+//Example 2
+#include <iostream>
+using namespace std;
+
+
+class Animal{
+    private:
+    string type;
+    public:
+    Animal():type("Animal"){}
+    virtual string getType() {
+        return type;
+    }
+};
+
+class Dog:public Animal{
+    private:
+        string type;
+    public:
+    Dog():type("Carlo"){}
+    string getType() override{
+        return type;
+    }
+};
+
+class Cat:public Animal{
+    private:
+    string type;
+    public:
+    Cat():type("Meow"){}
+
+    string getType() override{
+        return type;
+    }
+};
+
+void Print(Animal *animal){
+    cout<<"Animal :"<<animal->getType()<<endl;
+}
+
+int main(){
+   
+    Animal *animal1=new Animal();
+    Dog* dog=new Dog();
+    Cat* cat=new Cat();
+    Print(animal1);
+    Print(dog);
+    Print(cat);
+
+}
