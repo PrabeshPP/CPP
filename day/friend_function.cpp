4d9b37e55fc5ffc139de88f6c214f129d94731de
Prabesh <prabesh.bista369@gmail.com>
2021-12-18 09:43:52 +0545

    added two variables of the function

--- day/friend_function.cpp
+++ day/friend_function.cpp
@@ -1,20 +1,38 @@
 #include <iostream>
 using namespace std;
 
-class Distance{
+
+class ClassB;
+class ClassA{
     private:
-        int meter;
-    friend int addMeter(Distance);
-    public:
-    Distance():meter(5){}
+    int numA;
+    friend int add(ClassA,ClassB);
+public:
+ClassA():numA(10){}
 };
 
-int addMeter(Distance d){
-    d.meter+=5;
-    return d.meter;
+class ClassB{
+    private :
+        int numB;
+    friend int add(ClassA,ClassB);
+    public:
+        ClassB():numB(20){}
+};
+
+int add(ClassA a,ClassB b){
+    int sum;
+    sum=a.numA+b.numB;
+
+    return sum;
+
 }
 
+
 int main(){
-Distance d;
-cout<<"Distance : "<<addMeter(d)<<endl;
+    ClassA a;
+    ClassB b;
+    int sum=add(a,b);
+    cout<<"The sum of two objects variable is :"<<sum<<endl;
+
+
 }
