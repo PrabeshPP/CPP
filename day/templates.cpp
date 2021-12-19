9721a5183a055976c2037371bfbdf13e8e3b14b0
Prabesh <prabesh.bista369@gmail.com>
2021-12-19 20:34:58 +0545

    template,ex1

--- day/templates.cpp
+++ day/templates.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int main(){
 
+//class Template
+template<class T>
+
+class Number{
+    private:
+    //variable of type T
+    T num;
+    public:
+    Number(T n):num(n){}
+
+    T getNum(){
+        return num;
+    }
+};
+
+int main(){
+    Number<int> numberint(10);
+    Number<double> numberDouble(20.35);
+    cout<<"The integer number is "<<numberint.getNum()<<endl;
+    cout<<"The double number is "<<numberDouble.getNum()<<endl;
 }
