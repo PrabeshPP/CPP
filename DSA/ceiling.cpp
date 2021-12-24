d5aa047698754d6886604c48daa8d12886f785cf
Prabesh <prabesh.bista369@gmail.com>
2021-12-24 20:14:58 +0545

    ceiling algorithm

---
+++ DSA/ceiling.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+int Ceiling(int arr[],int target,int size){
+
+int start=0;
+int end=size;
+int mid=(start+end)/2;
+while(start<=end){
+    if(target>=arr[mid]){
+        return mid;
+
+    }else{
+        if(target<=arr[mid]){
+        end=mid-1;
+        mid=(start+end)/2;
+    }else{
+        start=mid+1;
+        mid=(start+end)/2;
+    }
+
+    }
+    
+
+}
+return start;
+
+
+    
+
+}
+
+
+
+int main(){
+    int arr[]={2,3,5,9,14,16,18};
+    int size=sizeof(arr)/sizeof(arr[0]);
+    int ret=Ceiling(arr,15,size);
+    cout<<ret;
+ 
+}
