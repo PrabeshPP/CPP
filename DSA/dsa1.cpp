5a428933703477d1b660fc049924878d9f0197dc
Prabesh <prabesh.bista369@gmail.com>
2021-12-24 20:10:08 +0545

    Binary search algorithm

--- DSA/dsa1.cpp
+++ DSA/dsa1.cpp
@@ -1,16 +1,19 @@
 #include<iostream>
 using namespace std;
 
+void binarySearch(int (&arr)[],int target){
+    cout<<sizeof(arr);
+
+
+
+    
+
+}
 
 
 
 int main(){
-    int arr[10]={22,3,4,66,7,8,99,1,101,8};
-    int target=9;
-    int arr2[1];
-    int count=0;
-    int length=sizeof(arr)/sizeof(arr[0]);
-    cout<<sizeof(arr[0])<<endl;
-    cout<<sizeof(arr);
-    
+    int arr[]={2,3,5,9,14,16,18};
+    binarySearch(arr,5);
+ 
 }
---
+++ DSA/tempCodeRunnerFile.cpp
@@ -0,0 +1,2 @@
+   // cout<<"Enter the number you want to search in an array";
+    // cin>>target;
