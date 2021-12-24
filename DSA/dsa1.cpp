307fca915826edd7a7ddf0da6f4aabec071f8d56
Prabesh <prabesh.bista369@gmail.com>
2021-12-24 19:40:36 +0545

    sizeof

--- DSA/dsa1.cpp
+++ DSA/dsa1.cpp
@@ -2,10 +2,15 @@
 using namespace std;
 
 
+
+
 int main(){
     int arr[10]={22,3,4,66,7,8,99,1,101,8};
     int target=9;
     int arr2[1];
     int count=0;
+    int length=sizeof(arr)/sizeof(arr[0]);
+    cout<<sizeof(arr[0])<<endl;
+    cout<<sizeof(arr);
     
 }
