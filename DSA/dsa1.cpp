cf6c27964d2281e9d4cddd87b47582ebe79fa502
Prabesh <prabesh.bista369@gmail.com>
2021-12-24 18:26:06 +0545

    dsa1

--- DSA/dsa1.cpp
+++ DSA/dsa1.cpp
@@ -5,15 +5,7 @@ using namespace std;
 int main(){
     int arr[10]={22,3,4,66,7,8,99,1,101,8};
     int target=9;
-    int arr2[];
+    int arr2[1];
     int count=0;
-    for (int i=o;i<=10;++i){
-        if(target<=arr[i]){
-            arr2[count]=arr[i];
-            count++;
-        }
-        
-    }
-    count=0;
-    cout<<arr2<<endl;
+    
 }
