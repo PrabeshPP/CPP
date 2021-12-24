438528f63f8efc3954d85eb40be7961bcd158571
Prabesh <prabesh.bista369@gmail.com>
2021-12-24 20:10:12 +0545

    Binary search algorithm

--- DSA/dsa1.cpp
+++ DSA/dsa1.cpp
@@ -1,9 +1,28 @@
 #include<iostream>
 using namespace std;
 
-void binarySearch(int (&arr)[],int target){
-    cout<<sizeof(arr);
+int binarySearch(int arr[],int target,int size){
+int start=0;
+int end=size;
+int mid=(start+end)/2;
+while(start<=end){
+    if(target==arr[mid]){
+        return mid;
 
+    }else{
+        if(target<=arr[mid]){
+        end=mid-1;
+        mid=(start+end)/2;
+    }else{
+        start=mid+1;
+        mid=(start+end)/2;
+    }
+
+    }
+    
+
+}
+return -1;
 
 
     
@@ -14,6 +33,8 @@ void binarySearch(int (&arr)[],int target){
 
 int main(){
     int arr[]={2,3,5,9,14,16,18};
-    binarySearch(arr,5);
+    int size=sizeof(arr)/sizeof(arr[0]);
+    int ret=binarySearch(arr,9,size);
+    cout<<ret;
  
 }
