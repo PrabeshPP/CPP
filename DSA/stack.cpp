6f04e6a6440036947a233ab6d1fe1573ccc5d28b
Prabesh <prabesh.bista369@gmail.com>
2021-12-31 19:03:41 +0545

    stack

---
+++ DSA/stack.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+//In stack,the elements that is enter at last pops first
+
+int size=0;
+class Stack{
+    private:
+        int Top;
+
+
+    public:
+        Stack(int _top){
+            Top=_top;
+
+
+        }
+    int push(){
+        return 0;
+
+    }
+    int pop(){
+        return 0;
+    }
+
+    
+};
+
+int main(){
+    int top=-1;
+    Stack stack=Stack(top);
+
+
+}
