33e5049a424bdcab5d7fbf008a747e6f400fb8f5
Prabesh <prabesh.bista369@gmail.com>
2022-01-08 17:32:18 +0545

    linked list

--- DSA/linked_list.cpp
+++ DSA/linked_list.cpp
@@ -1,1 +1,21 @@
 //Introduction to Linked List
+// We should start from the head
+// We do not have to declare the size of an array
+
+//Difference between array and Linked List
+/* 
+Time Complexity in Linked List is O(n);
+*/
+
+
+int main(){
+    struct Node
+    {
+        int data;
+        Node *link;
+        
+    };
+
+    
+
+}
